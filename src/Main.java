caf1ae32e442e4d66e5dca503a4bce1cd36d15ac
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2023-02-20 09:37:40 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+
+    }
+
+    public static String getString(String s) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(s + " : ");
+        return scanner.nextLine();
+    }
+
+}
